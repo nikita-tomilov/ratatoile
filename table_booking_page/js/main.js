1b3e5865a8f24fa54232098b138b893933b13701
Nikita Tomilov <programmer174@icloud.com>
2020-04-19 15:27:44 +0300

    table_booking_page: now you can book a table

--- table_booking_page/js/main.js
+++ table_booking_page/js/main.js
@@ -1,201 +1,206 @@
-(function($) {
+(function ($) {
 
-	'use strict';
+    'use strict';
 
-	// bootstrap dropdown hover
+    // bootstrap dropdown hover
 
-  // loader
-  var loader = function() {
-    setTimeout(function() { 
-      if($('#loader').length > 0) {
-        $('#loader').removeClass('show');
-      }
-    }, 1);
-  };
-  loader();
-
-	
-	$('nav .dropdown').hover(function(){
-		var $this = $(this);
-		$this.addClass('show');
-		$this.find('> a').attr('aria-expanded', true);
-		$this.find('.dropdown-menu').addClass('show');
-	}, function(){
-		var $this = $(this);
-			$this.removeClass('show');
-			$this.find('> a').attr('aria-expanded', false);
-			$this.find('.dropdown-menu').removeClass('show');
-	});
+    // loader
+    var loader = function () {
+        setTimeout(function () {
+            if ($('#loader').length > 0) {
+                $('#loader').removeClass('show');
+            }
+        }, 1);
+    };
+    loader();
 
 
-	$('#dropdown04').on('show.bs.dropdown', function () {
-	  console.log('show');
-	});
-
-	// home slider
-	$('.home-slider').owlCarousel({
-    loop:true,
-    autoplay: true,
-    margin:0,
-    animateOut: 'fadeOut',
-    animateIn: 'fadeIn',
-    nav:true,
-    autoplayHoverPause: true,
-    items: 1,
-    navText : ["<span class='ion-chevron-left'></span>","<span class='ion-chevron-right'></span>"],
-    responsive:{
-      0:{
-        items:1,
-        nav:false
-      },
-      600:{
-        items:1,
-        nav:false
-      },
-      1000:{
-        items:1,
-        nav:true
-      }
-    }
-	});
-
-  $('.home-slider-loop-false').owlCarousel({
-    loop:false,
-    autoplay: true,
-    margin:0,
-    animateOut: 'fadeOut',
-    animateIn: 'fadeIn',
-    nav:true,
-    autoplayHoverPause: true,
-    items: 1,
-    navText : ["<span class='ion-chevron-left'></span>","<span class='ion-chevron-right'></span>"],
-    responsive:{
-      0:{
-        items:1,
-        nav:false
-      },
-      600:{
-        items:1,
-        nav:false
-      },
-      1000:{
-        items:1,
-        nav:true
-      }
-    }
-  });
-
-	// owl carousel
-	var majorCarousel = $('.js-carousel-1');
-	majorCarousel.owlCarousel({
-    loop:true,
-    autoplay: true,
-    stagePadding: 7,
-    margin: 20,
-    animateOut: 'fadeOut',
-    animateIn: 'fadeIn',
-    nav: true,
-    autoplayHoverPause: true,
-    items: 3,
-    navText : ["<span class='ion-chevron-left'></span>","<span class='ion-chevron-right'></span>"],
-    responsive:{
-      0:{
-        items:1,
-        nav:false
-      },
-      600:{
-        items:2,
-        nav:false
-      },
-      1000:{
-        items:3,
-        nav:true,
-        loop:false
-      }
-  	}
-	});
-
-	// owl carousel
-	var major2Carousel = $('.js-carousel-2');
-	major2Carousel.owlCarousel({
-    loop:true,
-    autoplay: true,
-    stagePadding: 7,
-    margin: 20,
-    animateOut: 'fadeOut',
-    animateIn: 'fadeIn',
-    nav: true,
-    autoplayHoverPause: true,
-    items: 4,
-    navText : ["<span class='ion-chevron-left'></span>","<span class='ion-chevron-right'></span>"],
-    responsive:{
-      0:{
-        items:1,
-        nav:false
-      },
-      600:{
-        items:3,
-        nav:false
-      },
-      1000:{
-        items:4,
-        nav:true,
-        loop:false
-      }
-  	}
-	});
+    $('nav .dropdown').hover(function () {
+        var $this = $(this);
+        $this.addClass('show');
+        $this.find('> a').attr('aria-expanded', true);
+        $this.find('.dropdown-menu').addClass('show');
+    }, function () {
+        var $this = $(this);
+        $this.removeClass('show');
+        $this.find('> a').attr('aria-expanded', false);
+        $this.find('.dropdown-menu').removeClass('show');
+    });
 
 
-  $('.centernonloop').owlCarousel({
-    center: true,
-    items: 1,
-    loop: false,
-    margin:10,
-    dots: true,
-    responsive:{
-      600:{
-        items: 3
-      }
-    }
-  });
+    $('#dropdown04').on('show.bs.dropdown', function () {
+        console.log('show');
+    });
+
+    // home slider
+    $('.home-slider').owlCarousel({
+        loop: true,
+        autoplay: true,
+        margin: 0,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        nav: true,
+        autoplayHoverPause: true,
+        items: 1,
+        navText: ["<span class='ion-chevron-left'></span>", "<span class='ion-chevron-right'></span>"],
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 1,
+                nav: false
+            },
+            1000: {
+                items: 1,
+                nav: true
+            }
+        }
+    });
+
+    $('.home-slider-loop-false').owlCarousel({
+        loop: false,
+        autoplay: true,
+        margin: 0,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        nav: true,
+        autoplayHoverPause: true,
+        items: 1,
+        navText: ["<span class='ion-chevron-left'></span>", "<span class='ion-chevron-right'></span>"],
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 1,
+                nav: false
+            },
+            1000: {
+                items: 1,
+                nav: true
+            }
+        }
+    });
+
+    // owl carousel
+    var majorCarousel = $('.js-carousel-1');
+    majorCarousel.owlCarousel({
+        loop: true,
+        autoplay: true,
+        stagePadding: 7,
+        margin: 20,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        nav: true,
+        autoplayHoverPause: true,
+        items: 3,
+        navText: ["<span class='ion-chevron-left'></span>", "<span class='ion-chevron-right'></span>"],
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: true,
+                loop: false
+            }
+        }
+    });
+
+    // owl carousel
+    var major2Carousel = $('.js-carousel-2');
+    major2Carousel.owlCarousel({
+        loop: true,
+        autoplay: true,
+        stagePadding: 7,
+        margin: 20,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        nav: true,
+        autoplayHoverPause: true,
+        items: 4,
+        navText: ["<span class='ion-chevron-left'></span>", "<span class='ion-chevron-right'></span>"],
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 3,
+                nav: false
+            },
+            1000: {
+                items: 4,
+                nav: true,
+                loop: false
+            }
+        }
+    });
 
 
-	var contentWayPoint = function() {
-		var i = 0;
-		$('.element-animate').waypoint( function( direction ) {
-
-			if( direction === 'down' && !$(this.element).hasClass('element-animated') ) {
-				
-				i++;
-
-				$(this.element).addClass('item-animate');
-				setTimeout(function(){
-
-					$('body .element-animate.item-animate').each(function(k){
-						var el = $(this);
-						setTimeout( function () {
-							var effect = el.data('animate-effect');
-							if ( effect === 'fadeIn') {
-								el.addClass('fadeIn element-animated');
-							} else if ( effect === 'fadeInLeft') {
-								el.addClass('fadeInLeft element-animated');
-							} else if ( effect === 'fadeInRight') {
-								el.addClass('fadeInRight element-animated');
-							} else {
-								el.addClass('fadeInUp element-animated');
-							}
-							el.removeClass('item-animate');
-						},  k * 100);
-					});
-					
-				}, 100);
-				
-			}
-
-		} , { offset: '95%' } );
-	};
-	contentWayPoint();
+    $('.centernonloop').owlCarousel({
+        center: true,
+        items: 1,
+        loop: false,
+        margin: 10,
+        dots: true,
+        responsive: {
+            600: {
+                items: 3
+            }
+        }
+    });
 
 
+    var contentWayPoint = function () {
+        var i = 0;
+        $('.element-animate').waypoint(function (direction) {
+
+            if (direction === 'down' && !$(this.element).hasClass('element-animated')) {
+
+                i++;
+
+                $(this.element).addClass('item-animate');
+                setTimeout(function () {
+
+                    $('body .element-animate.item-animate').each(function (k) {
+                        var el = $(this);
+                        setTimeout(function () {
+                            var effect = el.data('animate-effect');
+                            if (effect === 'fadeIn') {
+                                el.addClass('fadeIn element-animated');
+                            } else if (effect === 'fadeInLeft') {
+                                el.addClass('fadeInLeft element-animated');
+                            } else if (effect === 'fadeInRight') {
+                                el.addClass('fadeInRight element-animated');
+                            } else {
+                                el.addClass('fadeInUp element-animated');
+                            }
+                            el.removeClass('item-animate');
+                        }, k * 100);
+                    });
+
+                }, 100);
+
+            }
+
+        }, {offset: '95%'});
+    };
+    contentWayPoint();
+
+    $('#datetimepicker1').datetimepicker({
+        lang: 'ru',
+        format: 'd.m.Y H:i',
+        minDate: 0,
+        allowTimes:['12:00','13:00','14:00','15:00','16:00','17:00','18:00','19:00','20:00','21:00','22:00','23:00']
+    });
 
 })(jQuery);
--- table_booking_page/js/reservation.js
+++ table_booking_page/js/reservation.js
@@ -1,15 +1,27 @@
 function reserveTable() {
     //pure govnokod going on here
     const datetimefrom = document.getElementById('datetimepicker1').value.toString();
-
     if (datetimefrom === "") {
         alert("Не задана дата")
+        return
+    }
+
+    const personName = document.getElementById('person_name').value.toString();
+    if (personName === "") {
+        alert("Не задано имя")
+        return
+    }
+
+    const personPhone = document.getElementById('person_phone').value.toString();
+    if (personPhone === "") {
+        alert("Не задан номер телефона")
         return
     }
 
     const longevity = document.getElementById('longevity').value.toString();
     const tabletype = document.getElementById('tabletype').value.toString();
     const message = document.getElementById('message').value.toString();
+    const personsCount = Number(document.getElementById('persons_count').value.toString());
 
     const dateFrom = datetimefrom.split(" ")[0];
     const timeFrom = datetimefrom.split(" ")[1];
@@ -29,23 +41,47 @@ function reserveTable() {
     if (tabletype === "У бара") {
         type = "NEAR_BAR";
     }
-    const comment = message;
-
-    const form = document.getElementById('reservation-form');
-    form.style.display = 'none';
-
-    const status = document.getElementById('reservation-status');
-    status.innerHTML = "<h2>Столик заказан, спасибо!</h2><a href=\"index.html\">На главную</a>"
-
     const requestBody = {
         from: tsFrom,
         to: tsTo,
-        seats: 2,
-        type: tabletype,
-        comment: comment
+        seats: personsCount,
+        type: type,
+        comment: message,
+        personName: personName,
+        personPhone: personPhone
     }
     const requestBodyJson = JSON.stringify(requestBody)
+    const url = window.location.origin + "/freeapi/1.0/reservation/create"
     fetch(url, {
-        
+        method: "POST",
+        body: requestBodyJson,
+        headers: {
+            'Content-Type': 'application/json',
+            'accept': 'application/json'
+        }
+    }).then((data) => {
+        if (data.ok) {
+            ok(data)
+        } else {
+            fail(data)
+        }
+    }).catch((data) => {
+        fail(data)
     })
+}
+
+function ok(data) {
+    data.json().then((data) => {
+        const form = document.getElementById('reservation-form');
+        form.style.display = 'none';
+        const status = document.getElementById('reservation-status');
+        const name = data.personName;
+        status.innerHTML = "<h2>Спасибо " + name + ", ваш столик заказан!</h2><a href=\"index.html\">На главную</a>"
+    });
+}
+
+function fail(data) {
+    data.json().then((data) => {
+        alert("Ошибка: " + data.message);
+    });
 }
